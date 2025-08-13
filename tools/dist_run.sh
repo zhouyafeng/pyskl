9aa89ac267e3b71d9264e5cb713a05d8213f2d02
zhouyf <zhouyf183@163.com>
2025-08-13 16:19:19 +0800

    feat(pyskl):[tools] replace 'python -m torch.distributed.launch' with 'torchrun'

--- tools/dist_run.sh
+++ tools/dist_run.sh
@@ -7,5 +7,5 @@ SCRIPT=$1
 GPUS=$2
 
 MKL_SERVICE_FORCE_INTEL=1 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$MASTER_PORT $SCRIPT ${@:3}
+torchrun --nproc_per_node=$GPUS --master_port=$MASTER_PORT $SCRIPT ${@:3}
 # Any arguments from the third one are captured by ${@:3}
--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -7,7 +7,8 @@ CONFIG=$1
 CHECKPOINT=$2
 GPUS=$3
 
-MKL_SERVICE_FORCE_INTEL=1 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+
 # Arguments starting from the forth one are captured by ${@:4}
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$MASTER_PORT \
+MKL_SERVICE_FORCE_INTEL=1 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+torchrun --nproc_per_node=$GPUS --master_port=$MASTER_PORT \
     $(dirname "$0")/test.py $CONFIG -C $CHECKPOINT --launcher pytorch ${@:4}
